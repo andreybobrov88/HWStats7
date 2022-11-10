1e0b2e081aa4c601c2d7df8505e2161df7bd21c2
Bobrov Andrey <andrey.bobrov88@bk.ru>
2022-11-10 14:55:24 +0700

    first commit

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,66 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAvSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+    }
+
+    @Test
+    public void shouldFindMaxMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maxSalesMonth(sales);
+        long expected = 7;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMinMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minSalesMonth(sales);
+        long expected = 8;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAbAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.aboveAverage(sales);
+        long expected = 5;
+    }
+
+    @Test
+    public void shouldBelAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.belowAverage(sales);
+        long expected = 5;
+    }
+
+
+}
